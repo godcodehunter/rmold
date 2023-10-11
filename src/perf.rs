d6257b2d7ac9ccb258ec90fad0ebf1839cc502a2
Dmitry Opokin <mamhigtt@gmail.com>
2023-10-11 17:43:24 +0700

    Changes

--- src/perf.rs
+++ src/perf.rs
@@ -2,15 +2,37 @@
 
 use std::{
     collections::HashMap,
+    panic::Location,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Mutex,
-    }, panic::Location,
+    },
 };
 use typed_builder::TypedBuilder;
 
 static COUNTERS: Mutex<Option<HashMap<String, &'static AtomicUsize>>> = Mutex::new(None);
-static COUNTERS_META: Mutex<Option<HashMap<String, CounterMeta>>> = Mutex::new(None); 
+static COUNTERS_META: Mutex<Option<HashMap<String, CounterMeta>>> = Mutex::new(None);
+
+pub fn counters() -> Vec<Counter> {
+    let mut counter_guard = COUNTERS.lock().unwrap();
+    let counter_map = counter_guard.get_or_insert_default();
+
+    let mut meta_guard = COUNTERS_META.lock().unwrap();
+    let meta_map = meta_guard.get_or_insert_default();
+
+    counter_map
+        .into_iter()
+        .map(|(key, value)| -> Counter {
+            let meta = &meta_map[key];
+
+            Counter {
+                name: meta.name.clone(),
+                description: meta.description.clone(),
+                value: AtomicUsize::new(value.load(Ordering::Relaxed)),
+            }
+        })
+        .collect::<Vec<_>>()
+}
 
 struct CounterMeta {
     name: String,
@@ -31,9 +53,9 @@ pub struct Counter {
 
 #[allow(non_camel_case_types)]
 impl<
-    __name: ::typed_builder::Optional<String>,
-    __description: ::typed_builder::Optional<String>,
-    __value: ::typed_builder::Optional<AtomicUsize>,
+        __name: ::typed_builder::Optional<String>,
+        __description: ::typed_builder::Optional<String>,
+        __value: ::typed_builder::Optional<AtomicUsize>,
     > CounterBuilder<(__name, __description, __value)>
 {
     #[track_caller]
@@ -52,7 +74,7 @@ impl<
             location.line(),
             location.column(),
         );
-        
+
         meta_map.entry(key.clone()).or_insert(CounterMeta {
             name: counter.name,
             description: counter.description,
@@ -65,23 +87,25 @@ impl<
     }
 }
 
-// pub struct Stats {
-//     instances: Vec<Counter>,
-// }
 
-// pub struct Timer {
-//     name: String,
-//     record: TimerRecord,
-// }
 
-// impl Timer {
-//     pub fn new(name: &str) -> Self {
-//         todo!()
-//     }
-// }
+pub struct Timer {
+    name: String,
+    record: TimerRecord,
+}
 
-// impl Drop for Timer {
-//     fn drop(&mut self) {
-//         self.record.stop()
-//     }
-// }
+impl Timer {
+    pub fn new(name: &str, parent: Option<&Timer>) -> Self {
+        
+    }
+
+    pub fn stop() {
+
+    }
+}
+
+impl Drop for Timer {
+    fn drop(&mut self) {
+        self.record.stop()
+    }
+}
